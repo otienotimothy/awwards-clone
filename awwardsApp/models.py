b5183943c1b510ade70a720ede130b4534a17e43
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-11 15:11:53 +0300

    Modify the Project model

--- awwardsApp/models.py
+++ awwardsApp/models.py
@@ -14,6 +14,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     projectUrl = models.CharField(max_length=200)
+    projectBy = models.ForeignKey(User, on_delete=models.CASCADE)
     
 
 class Rating(models.Model):
