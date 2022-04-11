102b3e609f520b615c9e07cec985d43403bdcab9
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-11 09:01:48 +0300

    Add login and signup view function

--- awwardsApp/views.py
+++ awwardsApp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signupUser(request):
+    return render(request, 'signup.html')
+
+def loginUser(request):
+    return render(request, 'login.html')
