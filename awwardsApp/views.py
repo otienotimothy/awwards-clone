9df0934df745a64f9b0d9dea28a3f5d7b557de2b
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-11 16:09:35 +0300

    Add functionality to signup and login in a user

--- awwardsApp/views.py
+++ awwardsApp/views.py
@@ -1,6 +1,9 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 from .forms import RegisterUserForm, LoginUserForm
+from .models import Profile
 
 
 # Create your views here.
@@ -8,11 +11,54 @@ def index(request):
     return render(request, 'index.html')
 
 def signupUser(request):
+
+    if request.user.is_authenticated:
+        return redirect(index)
+
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        user = form.save(commit=False)
+        if form.is_valid():
+            if User.objects.get(username=user.username.lower()).exists():
+                messages.error(request, f'A User with the username, {user.username}, Already exists')
+            elif User.objects.get(email=user.email.lower()).exists():
+                messages.error(request, f'A user with the email, {user.email}, Already exists')
+            else:
+                user.email = user.email.lower()
+                user.username = user.username.lower()
+                user.save()
+                userProfile = Profile(user=user)
+                userProfile.save()
+                login(request, user)
+                return redirect(index)
+
     form = RegisterUserForm()
     context = {'form': form}
     return render(request, 'signup.html', context)
 
 def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect(index)
+
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username'].lower()
+            password = form.cleaned_data['password']
+            try:
+                user_exist = User.objects.get(username=username)
+                if user_exist:
+                    user = authenticate(
+                        request, username=username, password=password)
+                    if user:
+                        login(request, user)
+                        return redirect(index)
+                    else:
+                        messages.error(request, 'Invalid username or password')
+            except:
+                messages.error(request, 'User does not exist, sign-up')
+
     form = LoginUserForm()
     context = {'form': form}
     return render(request, 'login.html', context)
