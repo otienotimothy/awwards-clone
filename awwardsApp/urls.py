1f4d77f51e9c32a48b24bb6fe93f7d50223b5137
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-12 06:15:08 +0300

    Add a route for a user's profile

--- awwardsApp/urls.py
+++ awwardsApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, signupUser, loginUser, logoutUser
+from .views import index, signupUser, loginUser, logoutUser, loadProfile
 
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', signupUser, name='signup'),
     path('login/', loginUser, name='login'),
-    path('logout/', logoutUser, name='logout')
+    path('logout/', logoutUser, name='logout'),
+    path('profile/<str:username>/', loadProfile, name='loadProfile')
 ]
