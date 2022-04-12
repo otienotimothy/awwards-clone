3f100ee09126d6b97943eb30b41c479332409e66
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-12 16:00:56 +0300

    Add rating project route

--- awwardsApp/urls.py
+++ awwardsApp/urls.py
@@ -1,6 +1,7 @@
+from unicodedata import name
 from django.urls import path
 
-from .views import index, signupUser, loginUser, logoutUser, loadProfile, editProfile, uploadProject
+from .views import index, signupUser, loginUser, logoutUser, loadProfile, editProfile, uploadProject, reviewProject
 
 urlpatterns = [
     path('', index, name='index'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('logout/', logoutUser, name='logout'),
     path('upload/', uploadProject, name='upload'),
     path('profile/<str:username>/', loadProfile, name='loadProfile'),
-    path('profile/edit/', editProfile, name='editProfile')
+    path('profile/edit/', editProfile, name='editProfile'),
+    path('rating/<int:projectId>/', reviewProject, name='rating' )
 ]
